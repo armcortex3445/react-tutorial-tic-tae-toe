698c29d61e3c3f876b1ec30fb66445cca4c9ce1a
armcortex3445 <wlghks5966@naver.com>
2024-02-14 17:04:37 +0900

    Add hilight winer square and draw message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import {useState} from 'react';
 
-function Square({value, onSquareClick}) {
+function Square({value, onSquareClick,hilight}) {
+
+  const className = hilight ? "hilight-square" : "square";
+
 
   return (
     <button 
-    className="square" onClick={onSquareClick} >
+    className={className} 
+    onClick={onSquareClick} 
+    >
       {value}
     </button>
   );
 }
 
-function calculateWinner(squares){
+function calculateResult(squares,move){
+  const ret = {winner : null , squares : null , isDraw : false};
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -24,22 +30,42 @@ function calculateWinner(squares){
 
   for (let i = 0; i < lines.length; i++){
     const [a,b,c] = lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
-      return squares[a];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+       ret.winner = squares[a];
+       ret.squares = lines[i];
+      break;
+    }
   }
 
-  return null;
+  if(move === 9 && !ret.winner){
+    ret.isDraw = true;
+  }
+
+  return ret;
+}
+
+function isHilighted(index,winnerSquares){
+
+  if(!winnerSquares) return false;
+
+  for(let i = 0; i < 3; i++){
+    if(index === winnerSquares[i]){
+      
+      return true;
+    }
+  }
+
 }
 
 
-function Board({xIsNext , squares, onPlay}) {
-  const winner = calculateWinner(squares);
+function Board({xIsNext , squares, onPlay, currentMove}) {
+  const result = calculateResult(squares,currentMove);
   let status;
   const size = 3;
   const array = Array.from({length : size} , (_, index) => index);
   
-  if(winner) {
-    status = "Winner: " + winner;
+  if(result.winner) {
+    status = "Winner: " + result.winner;
   }else{
     status = "Next Player: " + (xIsNext ? "X" : "O");
   }
@@ -48,7 +74,7 @@ function Board({xIsNext , squares, onPlay}) {
 
   function handleClick(i) {
 
-    if(squares[i] || calculateWinner(squares)) return;
+    if(squares[i] || calculateResult(squares).winner) return;
 
     const nextSquares = squares.slice();
 
@@ -63,6 +89,7 @@ function Board({xIsNext , squares, onPlay}) {
   return(
   <>
     <div className="status"> {status} </div>
+    {result.isDraw && <p className="status">Draw</p> }
     {array.map(row => (
         <div key={row} className="board-row">
           {array.map( col => (
@@ -70,6 +97,7 @@ function Board({xIsNext , squares, onPlay}) {
               key={col}
               value={squares[row*size+col]}
               onSquareClick={() => handleClick(row*size + col)}
+              hilight={isHilighted(row*size+col,result.squares)}
             />
           ))}
         </div>
@@ -125,7 +153,7 @@ export default function Game() {
   return (
     <div className="game"> 
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} currentMove={curMove} />
       </div>
       <div className="game-info">
         <ol>{currMoves}</ol>
@@ -138,4 +166,3 @@ export default function Game() {
   )
     
 }
-
